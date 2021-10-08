08717f3ed750073cbd55bdc321ae2df6d1c3fc56
erisvaldocorreia2 <erisvaldo.correia@rethink.dev>
2021-10-08 09:27:28 -0300

    configura constantes para o projeto

---
+++ lib/constants/font_styles.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import './colors.dart';
+
+class FontStyles {
+  static final cardTitle = GoogleFonts.poppins(
+    color: kThirdColor,
+    fontWeight: FontWeight.w700,
+    fontSize: 16,
+  );
+
+  static final cardSubTitle = GoogleFonts.poppins(
+    color: Colors.white,
+    fontWeight: FontWeight.w700,
+    fontSize: 18,
+  );
+
+  static final bodyText = GoogleFonts.poppins(
+    color: kPrimaryColor,
+    fontWeight: FontWeight.w700,
+    fontSize: 20,
+  );
+
+  static final listTitle = GoogleFonts.poppins(
+    color: kPrimaryColor,
+    fontSize: 20,
+  );
+
+  static final listSubTitle = GoogleFonts.poppins(
+    color: Colors.grey,
+    fontSize: 15,
+  );
+}
